086bc459e9f70fd3cfaf83ad723f9faba237a025
calistus-igwilo <ebunilo@yahoo.com>
2022-05-22 16:16:22 +0100

    fix previousHash

--- jsblockchain.js
+++ jsblockchain.js
@@ -1,11 +1,11 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block {
-    constructor(index, timestamp, data, previoushash = "") {
+    constructor(index, timestamp, data, previousHash = "") {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.previoushash = previoushash;
+        this.previoushash = previousHash;
         this.hash = this.calculateHash;
     }
 
@@ -14,9 +14,9 @@ class Block {
         return SHA256(
             this.index +
             this.timestamp +
-            this.previoushash +
-            JSON.stringify(this.data).toString
-        );
+            this.previousHash +
+            JSON.stringify(this.data)
+        ).toString();
     }
 }
 
@@ -39,6 +39,22 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash(); //calculate current block hash
         this.chain.push(newBlock); //add new block to the blockchain
     }
+
+    checkBlockChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 // create 2 new blocks
@@ -53,3 +69,13 @@ myBlockChain.addBlock(block1);
 myBlockChain.addBlock(block2);
 
 console.log(JSON.stringify(myBlockChain, null, 4));
+console.log(
+    "Validation check for Block Chain before hacking: " +
+    myBlockChain.checkBlockChainValid()
+);
+
+myBlockChain.chain[1].data = { mybalance: 5000 }; // a hacker changes data on the blockchain
+console.log(
+    "Validation check for Block Chain after hacking: " +
+    myBlockChain.checkBlockChainValid()
+);
