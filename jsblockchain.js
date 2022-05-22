96a7297997c2db423e5ed1cf09a4d3c255dd5533
calistus-igwilo <ebunilo@yahoo.com>
2022-05-22 12:35:27 +0100

    add new block to the blockchain

--- jsblockchain.js
+++ jsblockchain.js
@@ -1,18 +1,22 @@
 const sha256 = require("crypto-js/sha256");
 
 class Block {
-    constructor(index, timestamp, data, previoushash = '') {
+    constructor(index, timestamp, data, previoushash = "") {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previoushash = previoushash;
         this.hash = this.calculateHash;
-
     }
 
     calculateHash() {
         // Use SHA256 cryptographic function to generate the hash of this block
-        return SHA256(this.index + this.timestamp + this.previoushash + JSON.stringify(this.data).toString);
+        return SHA256(
+            this.index +
+            this.timestamp +
+            this.previoushash +
+            JSON.stringify(this.data).toString
+        );
     }
 }
 
@@ -23,6 +27,16 @@ class Blockchain {
     }
 
     createGenesisBlock() {
-        return new Block(0, "05/22/2022", "this is the genesis block", "0")
+        return new Block(0, "05/22/2022", "this is the genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash; //get previous block hash
+        newBlock.hash = newBlock.calculateHash(); //calculate current block hash
+        this.chain.push(newBlock); //add new block to the blockchain
     }
 }
